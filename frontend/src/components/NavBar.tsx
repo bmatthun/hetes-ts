c3fd44628ed2830eee19e6833fb24be538d82665
bence <bence.matuz1@gmail.com>
2025-02-05 11:13:07 +0100

    refactor: refactor NavBar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,24 +1,63 @@
 import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
-import { AppBar, Toolbar, Button, Box } from "@mui/material";
+import { AppBar, Toolbar, Button, Box, Typography } from "@mui/material";
 
 export default function NavBar() {
 
     console.log("dfs")
     return (
-        <AppBar position="sticky" sx={{ backgroundColor: "#433" }}>
-            <Toolbar sx={{ display: "flex", justifyContent: "space-between" }} >
-                <Link to="/" style={{ display: "flex", alignItems: "center", textDecoration: "none" }} >
-                    <Box component="img" src={logo} alt="Logo" sx={{ height: 40, mr: 2 }} />
-                </Link>
-                <Box>
-                    <Button color="inherit" component={Link} to="/about">About</Button>
-                    <Button color="inherit" component={Link} to="/services">Services</Button>
-                    <Button color="inherit" component={Link} to="/reservations">Reservations</Button>
-                    <Button color="inherit" component={Link} to="/order">Order now!</Button>
-                    <Button color="inherit" component={Link} to="/events">Events</Button>
-                    <Button color="inherit" component={Link} to="/contact">Contact</Button>
+        <AppBar position="sticky" sx={{ backgroundColor: "#FFD700" }}>
+            <Toolbar
+                sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    flexDirection: "column"
+                }}
+            >
+                <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+                    <Typography variant="h6" sx={{
+                        fontFamily: '"Oswald", "Druk Wide", "Arial Black", sans-serif',
+                        fontSize: "2rem",
+                        fontWeight: 700,
+                        letterSpacing: "0.2em",
+                        textTransform: "uppercase",
+                        color: "transparent",
+                        WebkitTextStroke: "1.5px black", 
+                    }}>
+                        HETES
+                    </Typography>
+                    <Box component="img" src={logo} alt="Logo" sx={{ height: 40 }} />
+                    <Typography variant="h6" sx={{
+                        fontFamily: '"Oswald", "Druk Wide", "Arial Black", sans-serif',
+                        fontSize: "2rem",
+                        fontWeight: 700,
+                        letterSpacing: "0.2em",
+                        textTransform: "uppercase",
+                        color: "black",
+                    }}>
+                        BISTRO
+                    </Typography>
                 </Box>
+
+                <Box sx={{
+                    fontFamily: '"Oswald", "Arial Black", sans-serif',
+                    fontSize: "2rem",
+                    fontWeight: 700,
+                    letterSpacing: "0.2em",
+                    textTransform: "uppercase",
+                    WebkitTextStroke: "1.5px white",  
+                }}>
+                    <Button color="inherit" component={Link} to="/about" sx={{ color: "black" }}>About</Button>
+                    <Button color="inherit" component={Link} to="/services" sx={{ color: "black" }}>Services</Button>
+                    <Button color="inherit" component={Link} to="/order" sx={{ color: "black" }}>Order now!</Button>
+                    <Button color="inherit" component={Link} to="/events" sx={{ color: "black" }}>Events</Button>
+                    <Button color="inherit" component={Link} to="/events" sx={{ color: "black" }}>Reservations</Button>
+                    <Button color="inherit" component={Link} to="/events" sx={{ color: "black" }}>Contact</Button>
+
+                </Box>
+
+
+
             </Toolbar>
         </AppBar>
     )
